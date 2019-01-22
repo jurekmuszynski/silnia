c4e6c64a1f4f66a1f3db8a89ebd84f28d275b9c6
george2m <jerzyk.kolekcjoner@gmail.com>
2019-01-22 16:35:29 +0100

    no message

---
+++ factorial/factorial/factorial.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+
+int num;
+
+using namespace std;
+
+int factorial(int n){
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		return n * factorial(n-1);
+	}
+}
+
+int main()
+{
+	cout << "enter your number: " << endl;
+	cin >> num;
+	cout << factorial(num) << endl;
+}
